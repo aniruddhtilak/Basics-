8a478d7970c4fc30ee9daaa9d3412e5f8d15c453
aniruddhtilak <141300258+aniruddhtilak@users.noreply.github.com>
2023-09-14 09:00:13 +0530

    Add files via upload

---
+++ Multiplication_table.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number,limit,multiplier;
+    cout<<"Enter the number - ";
+    cin>>number;
+    cout<<"Enter limit - ";
+    cin>>limit;
+    for(int multiplier=1;multiplier<=limit;i++){
+        cout<<number<<"*"<<multiplier<<"="<<i*multiplier<<endl;}
+}
